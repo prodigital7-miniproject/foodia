dbd6ac6be87e5d2e8d4d8747bdbb22cb512221ee
dk7648 <dk7648@korea.ac.kr>
2026-03-04 17:59:29 +0900

    Feat: 리뷰 rating값 정수 확인 기능 추가

--- src/lib/validators/review/review.ts
+++ src/lib/validators/review/review.ts
@@ -6,6 +6,7 @@ export const createReviewSchema = z.object({
   imgUrl: z.string().url("올바른 이미지 URL 형식이어야 합니다.").optional(),
   rating: z
     .number()
+    .int("평점은 정수여야 합니다.")
     .min(1, "평점을 입력하여 주세요.")
     .max(5, "평점은 5점이 최대입니다."),
   content: z.string().min(1, "리뷰 내용을 입력하여 주세요."),
